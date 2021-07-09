2c79d260af63788121e681d25e52ca3e153dcf27
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-07-09 18:26:23 -0500

    se realizan los ejercicios respecto al Binding y Event Binding, en el folder primer-proyect-angular.

--- Aprendizaje-Guia/primer-proyect-angular/src/app/empleado/empleado.component.ts
+++ Aprendizaje-Guia/primer-proyect-angular/src/app/empleado/empleado.component.ts
@@ -5,26 +5,53 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './empleado.component.html',
   styleUrls: ['./empleado.component.css']
 })
+
 export class EmpleadoComponent implements OnInit {
 
   nombre = 'Juan Camilo';
   private apellidos = 'González Duque';
   edad = 12;
   //private rol = 'Estudiante';
+  //Propiedades de Binding:
+  cajaCapturaTexto = false;//Recibe como valores true o false.
+  usuRegis = true;//Recibe como valores true o false.
+  textoInfoRegistrado = 'No hay usuarios registrados en el sistema';
 
   //Métodos get:
-  getApellidos(){
+  getApellidos() {
     return this.apellidos;
   }
-/*
-  getRol(){
-    return this.rol;
-  }
-*/
 
-  enviarRol(rol:String){
+  getUsuRegis() {
+    this.usuRegis = false;
+  }
+
+  //Métodos set:
+  setUsuRegisEvent() {//Trabajando con Eventos en Angular.
+    //alert('Usuario Registrado');
+    this.textoInfoRegistrado = 'Se ha registrado un usuario en el sistema';
+  }
+
+  setUsuRegisEvent2(eventRegis: Event) {//Trabajando con Eventos en Angular.
+    //alert('Usuario Registrado');
+    //this.textoInfoRegistrado = 'Se ha registrado un usuario en el sistema 2';
+    //alert("Capturando el evento: " + eventRegis.target);
+    if ((<HTMLInputElement>eventRegis.target).value == 'si') {
+      this.textoInfoRegistrado = 'Se ha registrado un usuario en el sistema 2';
+    }else{
+      this.textoInfoRegistrado = 'Gracias por visitarnos';
+    }
+  }
+  /*
+    getRol(){
+      return this.rol;
+    }
+  */
+
+  enviarRol(rol: String) {
     return rol;//si comento esta linea igual funciona, interesante jeje
   }
+
   constructor() {
 
   }
