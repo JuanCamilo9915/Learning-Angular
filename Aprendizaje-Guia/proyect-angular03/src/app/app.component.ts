c145fdd4b6d7bdc83398022ab7842964c80ce305
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-10-20 21:50:14 -0500

    se trabaja la directiva *ngIf dentro del proyecto: proyect-angular03.

--- Aprendizaje-Guia/proyect-angular03/src/app/app.component.ts
+++ Aprendizaje-Guia/proyect-angular03/src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'proyect-angular03';
+  titulo = 'Registro de Usuarios';
+  message = '';
+  message2 = '';
+  registrado = false;
+  nombre: String = '';
+  apellido: String = '';
+
+  registrarUsuario(){
+    this.registrado = true;
+    this.message = `Se ha registrado el usuario`;
+    this.message2 = 'Correctamente';
+  }
 }
