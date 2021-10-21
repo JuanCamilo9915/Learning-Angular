481df105c41f4c6fbc5ac2e12ca2523215089d95
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-10-20 19:38:06 -0500

    nuevo commit.

--- Aprendizaje-Guia/segundo-proyect-angular/src/app/app.component.ts
+++ Aprendizaje-Guia/segundo-proyect-angular/src/app/app.component.ts
@@ -5,6 +5,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'segundo-proyect-angular';
+  title = 'Mini Calculadora';
+  /*num1: number = 0;
+  num2: number = 0;*/
+  //Se puede dejar como number, si en el campo de texto se le asigna de tipo number.
+  num1: string = "0";
+  num2: string = "0";
+  result: number = 0;
+
+  //Métodos
+  sumar():void{
+    //this.result = this.num1 + this.num2; esto concatena
+    this.result = parseInt(this.num1) + parseInt(this.num2);
+  }
+
+  restar():void{
+    //this.result = this.num1 + this.num2; esto concatena
+    this.result = parseInt(this.num1) - parseInt(this.num2);
+  }
+
+  multiplicar():void{
+    //this.result = this.num1 + this.num2; esto concatena
+    this.result = parseInt(this.num1) * parseInt(this.num2);
+  }
+
+  dividir():void{
+    //this.result = this.num1 + this.num2; esto concatena
+    this.result = parseInt(this.num1) / parseInt(this.num2);
+  }
+
 }
